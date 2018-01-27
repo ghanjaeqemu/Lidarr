cd942104706307d38f7e7823d291075e972ba166
Qstick <qstick@gmail.com>
2018-01-27 01:36:47 -0500

    New: Import Lists Base

---
+++ src/NzbDrone.Core/ImportLists/ImportListDefinition.cs
@@ -0,0 +1,20 @@
+using NzbDrone.Core.ThingiProvider;
+
+namespace NzbDrone.Core.ImportLists
+{
+    public class ImportListDefinition : ProviderDefinition
+    {
+        public bool EnableAutomaticAdd { get; set; }
+        public bool EnableInteractiveAdd { get; set; }
+
+        public bool ShouldMonitor { get; set; }
+        public int ProfileId { get; set; }
+        public int LanguageProfileId { get; set; }
+        public int MetadataProfileId { get; set; }
+        public string RootFolderPath { get; set; }
+
+        public override bool Enable => EnableAutomaticAdd || EnableInteractiveAdd;
+
+        public ImportListStatus Status { get; set; }
+    }
+}
